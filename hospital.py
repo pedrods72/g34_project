c8fa2b0c668070a29caf5125de93e83ff382cc45
pedrods72 <pedrodinissantos2007@gmail.com>
2026-04-25 16:47:50 +0100

    Update do hospital.py

--- hospital.py
+++ hospital.py
@@ -5,10 +5,11 @@ Created on Sat Apr 25 15:33:26 2026
 @author: User
 """
 
-# @author: Stockler (baseado no template de António Brito)
-# objective: class Hospital derived from Gclass
+# @author: Stockler (baseado no template da class Person do prof. António Brito), revisto por: Pedro
+# objetivo: class Hospital derivada da Gclass
 
-from gclass import Gclass
+from classes.gclass import Gclass
+import datetime
 
 class Hospital(Gclass):
     obj = dict()
@@ -16,22 +17,23 @@ class Hospital(Gclass):
     pos = 0
     sortkey = ''
     
-    # Lista de atributos: o id tem de ser o primeiro
-    # No teu CSV tens hospital_id e name
-    att = ['_id', '_name']
+    att = ['_id', '_name', '_creation_date']
     
-    # Título para as listagens
     header = 'Hospitais'
     
-    # Descrição dos campos
-    des = ['Id', 'Nome do Hospital']
+    des = ['Id', 'Nome do Hospital', 'Data de Criação']
     
-    def __init__(self, id, name):
+    def __init__(self, id, name, creation_date):
         super().__init__()
-        # O get_id vem da Gclass que copiaste antes
+        
         id = Hospital.get_id(id)
         self._id = id
         self._name = name
+        
+        if isinstance(creation_date, str):
+            self._creation_date = datetime.date.fromisoformat(creation_date)
+        else:
+            self._creation_date = creation_date
         
         # Guarda nas listas da classe para o motor funcionar
         Hospital.obj[id] = self
@@ -54,3 +56,13 @@ class Hospital(Gclass):
     @name.setter
     def name(self, name):
         self._name = name
+
+ # Getter e Setter para a Data de Criação
+    @property
+    def creation_date(self):
+        return self._creation_date
+
+    @creation_date.setter
+    def creation_date(self, creation_date):
+        self._creation_date = creation_date
+
