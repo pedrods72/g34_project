6df45f1594cbd94179e54639d3d7d2f8eec75bdc
pedrods72 <pedrodinissantos2007@gmail.com>
2026-04-25 16:53:26 +0100

    Update da classe utilization.py

--- utilization.py
+++ utilization.py
@@ -3,8 +3,8 @@ Created on Sat Apr 25 16:19:29 2026
 
 @author: tiago
 """
+#Revisto por: Pedro
 
-# Class Utilization - generic version with inheritance
 from classes.gclass import Gclass
 import datetime
 
@@ -13,28 +13,40 @@ class Utilization(Gclass):
     lst = list()
     pos = 0
     sortkey = ''
-    # Attribute names list, identifier attribute must be the first one and called 'id'
+    
+
     att = ['_id', '_department_id', '_device_id', '_utilization_date', '_amount']
-    # Class header title
+    
     header = 'Utilizations'
-    # field description for use in, for example, input form
+    
     des = ['Id', 'Department Id', 'Device Id', 'Utilization Date', 'Amount']
-    # Constructor: Called when an object is instantiated
+    
+
     def __init__(self, id, department_id, device_id, utilization_date, amount):
         super().__init__()
-        # Object attributes
+        
         id = Utilization.get_id(id)
         self._id = id
-        self._department_id = int(department_id)
+        
+
+        self._department_id = int(department_id) if department_id else None
+        
         self._device_id = int(device_id)
-        self._utilization_date = datetime.date.fromisoformat(utilization_date)
+        
+        # Proteção para o formato da data (lida tanto com DD/MM/YYYY como YYYY-MM-DD)
+        if isinstance(utilization_date, str) and '/' in utilization_date:
+            self._utilization_date = datetime.datetime.strptime(utilization_date, '%d/%m/%Y').date()
+        else:
+            self._utilization_date = datetime.date.fromisoformat(str(utilization_date))
+            
+
         self._amount = int(amount)
-        # Add the new object to the dictionary of objects
+        
         Utilization.obj[id] = self
-        # Add the id to the list of object ids
         Utilization.lst.append(id)
 
-    # id property getter method
+    # --- PROPERTIES ---
+
     @property
     def id(self):
         return self._id
@@ -42,7 +54,6 @@ class Utilization(Gclass):
     def id(self, id):
         self._id = id
 
-    # department_id property getter method
     @property
     def department_id(self):
         return self._department_id
@@ -50,7 +61,6 @@ class Utilization(Gclass):
     def department_id(self, department_id):
         self._department_id = department_id
 
-    # device_id property getter method
     @property
     def device_id(self):
         return self._device_id
@@ -58,7 +68,6 @@ class Utilization(Gclass):
     def device_id(self, device_id):
         self._device_id = device_id
 
-    # utilization_date property getter method
     @property
     def utilization_date(self):
         return self._utilization_date
@@ -66,7 +75,6 @@ class Utilization(Gclass):
     def utilization_date(self, utilization_date):
         self._utilization_date = utilization_date
 
-    # amount property getter method
     @property
     def amount(self):
         return self._amount
