ac0da4deae914959da988a17ef8014ae13b45008
NunoStockler <up202506539@up.pt>
2026-05-29 19:03:31 +0100

    Estrutura inicial da análise

--- analise_hospitalar.py
+++ analise_hospitalar.py
@@ -1,1 +1,16 @@
 # Módulo de análise estatística de dados hospitalares - Versão inicial
+import pandas as pd
+import plotly.express as px
+import sqlite3
+import os
+
+# 1. Configurar o caminho para a base de dados
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(base_dir, 'dados', 'HospitalData.db')
+
+def carregar_dados(tabela):
+    """Função para ler qualquer tabela da base de dados usando Pandas"""
+    conn = sqlite3.connect(db_path)
+    df = pd.read_sql_query(f"SELECT * FROM {tabela}", conn)
+    conn.close()
+    return df
